c98fad4781e08c518278f7568b9614b911dc733f
Matt Carlson <matthewthomascarlson@gmail.com>
2017-09-19 13:06:05 -0700

    handle form submittal

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,14 +11,21 @@ class PostsNew extends Component {
         type="text"
           {...field.input}
         />
+        {field.meta.error}
       </div>
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
       <div>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <Field
             label="Title"
             name='title'
@@ -34,6 +41,7 @@ class PostsNew extends Component {
             name='content'
             component={this.renderField}
           />
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
       </div>
     );
