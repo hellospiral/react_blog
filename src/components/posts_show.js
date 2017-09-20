eb1da480fae0f2d7b3134dda665f328a63f4665d
Matt Carlson <matthewthomascarlson@gmail.com>
2017-09-19 19:10:05 -0700

    allow deletion of a post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -16,9 +24,16 @@ class PostsShow extends Component {
     }
     return (
       <div>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
+        <Link to="/" className="btn btn-primary">Home</Link>
       </div>
     );
   }
@@ -28,4 +43,4 @@ function mapStateToProps( { posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
